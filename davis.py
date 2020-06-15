fd3f424420c2b2c7ead65b1950aa41bd407ec9f6
IYERADavid <iyeradavid@gmail.com>
2020-06-15 12:14:20 +0200

    updated commit

--- davis.py
+++ davis.py
@@ -1,9 +1,8 @@
 import calendar
+import time 
 
 yy=int(input('enter a year:'))
 mm=int(input('enter a month:'))
 print (calendar.month(yy,mm))
 
-
-print('secv')
-print(4*5)
+print ('we are on ' + str(time.asctime()))
